378bc659896c54df81c4751b5b93c4e9ec4ce9ba
Everson de Oliveira <eor1309@gmail.com>
2023-04-08 15:47:03 -0300

    Inserção de Bootcamp e Devs, e monitoramento de progresso.

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,60 @@ public class Main {
         mentoria.setDescricao("Mentoria sobre Java");
         mentoria.setData(LocalDate.now());
 
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcampFull =  new Bootcamp();
+        bootcampFull.setNome("Bootcamp Fullstack");
+        bootcampFull.setDescricao("Descrição sobre o Bootcamp Fullstack");
+        bootcampFull.getConteudos().add(curso1);
+        bootcampFull.getConteudos().add(curso2);
+        bootcampFull.getConteudos().add(mentoria);
+
+        Bootcamp bootcampJs =  new Bootcamp();
+        bootcampJs.setNome("Bootcamp javascript Developer");
+        bootcampJs.setDescricao("Descrição sobre o Bootcamp Javascript Developer");
+        bootcampJs.getConteudos().add(curso2);
+
+        Bootcamp bootcampJava =  new Bootcamp();
+        bootcampJava.setNome("Bootcamp java Developer");
+        bootcampJava.setDescricao("Descrição sobre o Bootcamp java Developer");
+        bootcampJava.getConteudos().add(curso1);
+        bootcampJava.getConteudos().add(mentoria);
+
+        System.out.println("---------------------------------------------");
+        Dev devEverson  = new Dev();
+        devEverson.setNome("Everson");
+        devEverson.inscrevendoBootcamp(bootcampFull);
+        System.out.println("Conteúdos inscritos Everson " + devEverson.getConteudosInscritos());
+        devEverson.progredir();
+        System.out.println("/////////////////////////");
+        System.out.println("Conteúdos inscritos Everson " + devEverson.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Everson " + devEverson.getConteudosConcluidos());
+        System.out.println("XP: "+ devEverson.calcularXpTotal());
+
+        System.out.println("---------------------------------------------");
+        Dev devMarcela = new Dev();
+        devMarcela.setNome("Marcela");
+        devMarcela.inscrevendoBootcamp(bootcampJava);
+        System.out.println("Conteúdos inscritos Marcela " + devMarcela.getConteudosInscritos());
+        devMarcela.progredir();
+        System.out.println("/////////////////////////");
+        System.out.println("Conteúdos inscritos Marcela " + devMarcela.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Marcela " + devMarcela.getConteudosConcluidos());
+        System.out.println("XP: "+ devMarcela.calcularXpTotal());
+
+        System.out.println("---------------------------------------------");
+        Dev devTheo = new Dev();
+        devTheo.setNome("Théo");
+        devTheo.inscrevendoBootcamp(bootcampJs);
+        System.out.println("Conteúdos inscritos Théo " + devTheo.getConteudosInscritos());
+        devTheo.progredir();
+        System.out.println("/////////////////////////");
+        System.out.println("Conteúdos inscritos Théo " + devTheo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Théo " + devTheo.getConteudosConcluidos());
+        System.out.println("XP: "+ devTheo.calcularXpTotal());
+
     }
 }
